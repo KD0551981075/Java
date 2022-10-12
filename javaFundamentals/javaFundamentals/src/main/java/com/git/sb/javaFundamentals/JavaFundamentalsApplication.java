de973fc1f94a74eb5d6e1d25e9601fe1fb758e37
KD0551981075 <75532850+KD0551981075@users.noreply.github.com>
2022-10-12 18:57:24 +0530

    Add files via upload
    This repo contains all the topics related to java fundamentals

---
+++ javaFundamentals/javaFundamentals/src/main/java/com/git/sb/javaFundamentals/JavaFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.git.sb.javaFundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFundamentalsApplication.class, args);
+	}
+
+}
